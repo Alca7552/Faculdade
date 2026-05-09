934edded85e593caad0d4a794b1a7bcb4d91c550
Alca7552 <acla0999.mg@gmail.com>
2026-05-08 21:31:59 -0300

    Atualização do codigo para algo mais completo

--- atividade2.cpp
+++ atividade2.cpp
@@ -1,15 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int main(){
-	
+int main() {
+
 	int num1;
-	
+
 	printf("Digite um numero: ");
-	scanf("%d",&num1);
-	
-	if(num1 % 2 == 0)
-		printf("Par");
+	scanf("%d", &num1);
+
+	if (num1 % 2 == 0)
+		printf("O numero %d e um numero Par", num1);
 	else
-		printf("Impar");
-		
+		printf("O numero %d e um numero Impar", num1);
+
+}
 }
